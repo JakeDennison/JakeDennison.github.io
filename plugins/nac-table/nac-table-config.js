56666dc20826ef3e20ad2a01058375d875f6bbe2
JSDenintex <jake.dennison@nintex.com>
2022-12-28 14:57:10 +0000

    added contract

---
+++ plugins/nac-table/nac-table-config.js
@@ -0,0 +1,26 @@
+import './query-assigned-elements-214d6340.js';
+import { Z as ZincVersion } from './zinc-api-f0859f9f.js';
+
+const config = {
+  title: 'nac-table',
+  fallbackDisableSubmit: false,
+  description: 'Renders a table based on json data',
+  iconUrl: 'rich-text',
+  groupName: 'Visual',
+  pluginAuthor: 'Jake Dennison',
+  version: ZincVersion.CurrentVersion,
+  properties: {
+    Object: {
+      type: 'string',
+      title: 'object as a string',
+      description: 'Store the object in a variable to use here'
+    },
+  },
+  standardProperties: {
+    readOnly: true,
+    required: true,
+    description: true
+  }
+};
+
+export { config };
--- plugins/nac-table/nac-table.js
+++ plugins/nac-table/nac-table.js
@@ -1,13 +1,30 @@
 import { LitElement, html } from 'lit-element';
+import './query-assigned-elements-214d6340.js';
+import { Z as ZincVersion } from './zinc-api-f0859f9f.js';
+
+const config = {
+  title: 'nac-table',
+  fallbackDisableSubmit: false,
+  description: 'Renders a table based on json data',
+  iconUrl: 'rich-text',
+  groupName: 'Visual',
+  pluginAuthor: 'Jake Dennison',
+  version: ZincVersion.CurrentVersion,
+  properties: {
+    Object: {
+      type: 'string',
+      title: 'object as a string',
+      description: 'Store the object in a variable to use here'
+    },
+  },
+  standardProperties: {
+    readOnly: true,
+    required: true,
+    description: true
+  }
+};
 
 class MyTable extends LitElement {
-  static async getMetaConfig() {
-    return {
-      controlName: 'nac-table',
-      fallbackDisableSubmit: false,
-      version: '1.0',
-    };
-  }
   async loadData() {
     try {
       const response = await fetch('data.json');
