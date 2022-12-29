6e9b42f0d0909c4521d96021ae9a76d53e7281fc
JSDenintex <jake.dennison@nintex.com>
2022-12-29 10:55:17 +0000

    updated test

--- plugins/lit-test.js
+++ plugins/lit-test.js
@@ -1,21 +1,75 @@
-import { html,LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
-​
-// define the component
-export class CustomInput extends LitElement {
-  // return a promise for contract changes.
-  static async getMetaConfig() {
-    return {
-      controlName: 'Custom input',
-      fallbackDisableSubmit: false,
-      version: '1.2',
-    };
-  }
-​
-  render() {
-    return html`<input placeholder="custom input" />`;
-  }
+import {css, html, LitElement, styleMap} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+
+export class ZwcIframe extends LitElement {
+    // Define scoped styles right with your component, in plain CSS
+    static styles = css`
+      :host {
+        height: 100%;
+        width: 100%;
+        display: block;
+      }
+
+      .frame {
+        display: inline-block;
+        height: 100%;
+        width: 100%;
+        background-color: transparent;
+        border: none;
+      }
+    `;
+
+    static getMetaConfig() {
+        // plugin contract information
+        return {
+            controlName: 'IFrame-new',
+            fallbackDisableSubmit: false,
+            description: 'IFrame component which can render url view with the frame',
+            iconUrl: "one-line-text",
+            groupName: 'Visual',
+            version: '1.3',
+            properties: {
+                src: {
+                    type: 'string',
+                    title: 'Source URL',
+                    description: 'URL of the iframe, please note many sites block been rendered in iframes'
+                },
+                height: {
+                    type: 'string',
+                    title: 'Height',
+                    description: 'Height of the component',
+                }
+            },
+            standardProperties: {
+                readOnly: true,
+                required: true,
+                description: true,
+            }
+        };
+    }
+
+    static properties = {
+        name: 'Hello',
+        title: 'Hello',
+        src: 'https://www.wikipedia.org/',
+        height: '100%'
+    }
+
+    // Render the UI as a function of component state
+    render() {
+        let styles = {height: this.height};
+
+        return html`
+            <iframe
+                    class="frame"
+                    style=${styleMap(styles)}
+                    name=${this.name}
+                    allow="geolocation *; microphone; camera"
+                    title=${this.title}
+                    src=${this.src}
+            ></iframe>`;
+    }
 }
-​
-// registering the web component
-const elementName = 'custom-input';
-customElements.define(elementName, CustomInput);
+
+// registering the web component.
+const elementName = 'zwc-iframe-new';
+customElements.define(elementName, ZwcIframe);
