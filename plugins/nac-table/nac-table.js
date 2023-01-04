deb3235f49cde6b485d9969242e1ab2f19d71fd2
JSDenintex <jake.dennison@nintex.com>
2023-01-04 09:24:39 +0000

    changed to export

--- plugins/nac-table/nac-table.js
+++ plugins/nac-table/nac-table.js
@@ -24,7 +24,7 @@ const config = {
   }
 };
 
-class MyTable extends LitElement {
+export class MyTable extends LitElement {
   static async getMetaConfig() {
     return {
       controlName: 'nac-table',
