c85d2b13176c67e2ffdbb851f5be32ab722a5710
JSDenintex <jake.dennison@nintex.com>
2023-01-04 15:27:01 +0000

    made it editable i hope

--- plugins/nac-table/nac-table.js
+++ plugins/nac-table/nac-table.js
@@ -1,12 +1,11 @@
 import {css, html, LitElement, styleMap} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
-
 export class MyTable extends LitElement {
   static async getMetaConfig() {
     return {
       title: 'nac-table',
       fallbackDisableSubmit: false,
-      version: '1.2',
+      version: '1.3',
     };
   }
   
@@ -14,6 +13,9 @@ export class MyTable extends LitElement {
     try {
       const response = await fetch('https://jsdenintex.github.io/plugins/nac-table/data.json');
       this.data = await response.json();
+      this.data.forEach(row => {
+        row.edit = false;
+      });
       this.requestUpdate();
     } catch (error) {
       console.error(error);
@@ -25,43 +27,68 @@ export class MyTable extends LitElement {
     this.loadData();
   }
 
+  updateRow(e, row, key) {
+    row[key] = e.target.value;
+  }
+
+  editRow(row) {
+    row.edit = true;
+    this.requestUpdate();
+  }
+
+  cancelEdit(row) {
+    row.edit = false;
+    this.requestUpdate();
+  }
+
+  async saveRow(row) {
+    try {
+      const response = await fetch('https://jsdenintex.github.io/plugins/nac-table/data.json', {
+        method: 'PUT',
+        body: JSON.stringify(row),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if (response.ok) {
+        row.edit = false;
+        this.requestUpdate();
+      } else {
+        console.error('Failed to save row');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
     if (!this.data) {
       return html``;
     }
-  
+
     const rows = this.data.map(row => html`
-      <tr>
-        ${Object.values(row).map(cell => html`<td>${cell}</td>`)}
-      </tr>
+      ${row.edit ? html`
+        <tr>
+          ${Object.keys(row).map(key => html`
+            <td>
+              <input type="text" value=${row[key]} @input=${e => this.updateRow(e, row, key)}>
+            </td>
+          `)}
+          <td>
+            <button @click=${() => this.saveRow(row)}>Save</button>
+            <button @click=${() => this.cancelEdit(row)}>Cancel</button>
+          </td>
+        </tr>
+      ` : html`
+        <tr>
+          ${Object.values(row).map(cell => html`<td>${cell}</td>`)}
+          <td>
+            <button @click=${() => this.editRow(row)}>Edit</button>
+          </td>
+        </tr>
+      `}
     `);
-
-    const headers = Object.keys(this.data[0]).map(header => html`<th>${header}</th>`);
-
-    return html`
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            ${headers}
-          </tr>
-        </thead>
-        <tbody>
-          ${rows}
-        </tbody>
-      </table>
-    `;
   }
 }
-
-function setInputValues() {
-  const inputs = document.querySelectorAll('.nx-input-control');
-  inputs.forEach(input => {
-    input.value = 'jake';
-  });
-}
-
-setInputValues();
 const elementName = 'nac-table';
 customElements.define('nac-table', MyTable);
