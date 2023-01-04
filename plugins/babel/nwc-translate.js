02e7c1c9bc300ed94a9500c51c59ded0c628782f
JSDenintex <jake.dennison@nintex.com>
2023-01-04 15:05:23 +0000

    simplified

--- plugins/babel/nwc-translate.js
+++ plugins/babel/nwc-translate.js
@@ -1,58 +1,27 @@
 import {css, html, LitElement, styleMap} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
-export class TranslateMod extends LitElement {
-
-    static getMetaConfig() {
-        // plugin contract information
-        return {
-            title: 'translate-mod',
-            fallbackDisableSubmit: false,
-            description: 'Non interactable plugin to translate elements of Nintex Forms',
-            iconUrl: "multiline-text",
-            groupName: 'Translators',
-            version: '1.0',
-            properties: {
-                Signature: {
-                    type: 'string',
-                    title: 'Select to sign',
-                    description: 'Replacement text for "Select to sign" located in the signature control'
-                },
-            },
-            standardProperties: {
-                readOnly: true,
-                required: true,
-                description: true,
-            }
-        };
-    }
-
-    render() {
-        return html`
-          <!-- element template -->
-          <p>Translation</p>
-          <script>
-          console.log('render is working');
-          </script>
-        `;
-      }
-}
-
-function replaceH5(newText) {
-    // Get all h5 elements with the "d-print-none ng-star-inserted" class
-    var h5Elements = document.querySelectorAll('h5.d-print-none.ng-star-inserted');
-    
-    // Loop through all h5 elements
-    for (var i = 0; i < h5Elements.length; i++) {
-      // Replace the inner HTML of the current h5 element with the new text
-      h5Elements[i].innerHTML = newText;
-    }
+export class Placeholder extends LitElement {
+  static async getMetaConfig() {
+    return {
+      title: 'ph-translate',
+      fallbackDisableSubmit: false,
+      version: '1.2',
+    };
   }
   
-  window.addEventListener('load', function() {
-    replaceH5('This is the new h5 text');
+  render() {
+    return html`
+    `;
+  }
+}
+
+function setInputValues() {
+  const elements = document.querySelectorAll('h5.d-print-none.ng-star-inserted');
+  elements.forEach(element => {
+    element.innerHTML = 'Klik hier om te ondertekenen';
   });
+}
 
-// registering the web component.
-customElements.define('translate-mod', TranslateMod);
-
-
+setInputValues();
+const elementName = 'ph-translate';
+customElements.define('ph-translate', Placeholder);
