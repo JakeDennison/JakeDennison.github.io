7c9f1f2385f7e7a5e1264f07934f01cdf3ae1762
JSDenintex <jake.dennison@nintex.com>
2023-01-05 10:20:41 +0000

    loading from data object

--- plugins/nac-table/nac-table.js
+++ plugins/nac-table/nac-table.js
@@ -1,64 +1,52 @@
 import {css, html, LitElement, styleMap} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
-
 export class MyTable extends LitElement {
-
-    static getMetaConfig() {
-      // plugin contract information
-      return {
-          title: 'nac-table',
-          fallbackDisableSubmit: false,
-          description: 'Display object as a table',
-          iconUrl: "group-control",
-          groupName: 'Visual',
-          version: '1.3',
-          properties: {
-              dataobject: {
-                  type: 'string',
-                  title: 'Object',
-                  description: 'Test'
-              },
-          },
-          standardProperties: {
-              readOnly: true,
-              required: true,
-              description: true,
-          }
-      };
+  static getMetaConfig() {
+    // plugin contract information
+    return {
+      title: 'nac-table',
+      fallbackDisableSubmit: false,
+      description: 'Display object as a table',
+      iconUrl: "group-control",
+      groupName: 'Visual',
+      version: '1.3',
+      properties: {
+        dataobject: {
+          type: 'string',
+          title: 'Object',
+          description: 'Test'
+        },
+      },
+      standardProperties: {
+        readOnly: true,
+        required: true,
+        description: true,
+      }
+    };
   }
   
   static properties = {
     dataobject: 'test'
   }
 
-  async loadData() {
-    try {
-      const response = await fetch('https://jsdenintex.github.io/plugins/nac-table/data.json');
-      this.data = await response.json();
-      this.requestUpdate();
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
   constructor() {
     super();
-    this.loadData();
   }
 
   render() {
-    if (!this.data) {
+    if (!this.dataobject) {
       return html``;
     }
-  
-    const rows = this.data.map(row => html`
+
+    const data = JSON.parse(this.dataobject);
+    const rows = data.map(row => html`
       <tr>
         ${Object.values(row).map(cell => html`<td>${cell}</td>`)}
       </tr>
     `);
 
     const dost = this.dataobject;
-    const headers = Object.keys(this.data[0]).map(header => html`<th>${header}</th>`);
+    const headers = Object.keys(data[0]).map(header => html`<th>${header}</th>`);
 
     return html`
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
