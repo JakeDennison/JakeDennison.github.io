c6ecd223eb5f0c110450610ee040eec78b02119b
JSDenintex <jake.dennison@nintex.com>
2023-01-09 10:15:40 +0000

    testing address

--- plugins/nac-translate/nac-translate-legacy.js
+++ plugins/nac-translate/nac-translate-legacy.js
@@ -1,7 +1,5 @@
 import {css, html, LitElement, styleMap} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
-
-
 export class Placeholder extends LitElement {
   static getMetaConfig() {
     // plugin contract information
@@ -12,23 +10,11 @@ export class Placeholder extends LitElement {
       iconUrl: "group-control",
       groupName: 'Languages',
       version: '1.3',
-      properties: {
-        dataobject: {
-          type: 'string',
-          title: 'Object',
-          description: 'Test'
-        },
-      },
       standardProperties: {
         readOnly: true,
-        required: true,
         description: true,
       }
     };
-  }
-  
-  static properties = {
-    dataobject: 'test'
   }
 
   render() {
@@ -38,12 +24,29 @@ export class Placeholder extends LitElement {
 
 }
 
+// Translations here
+// Signature control
+const signhere = "Klicken Sie hier, um zu unterschreiben";
+// Address control
+const enteradd = "Gib eine Adresse ein";
+
+
+// Translate Signature Field
 function setInputValues() {
   const elements = document.querySelectorAll('h5.d-print-none.ng-star-inserted');
   elements.forEach(element => {
-    element.innerHTML = 'Klicken Sie hier, um zu unterschreiben.';
+    element.innerHTML = signhere;
   });
 }
+
+// Translate Address Field
+function setInputValues() {
+  const elements = document.querySelectorAll('.nx-address-input');
+  elements.forEach(element => {
+    element.setAttribute = ('Enter an address',enteradd);
+  });
+}
+
 
 setInputValues();
 const elementName = 'translate-legacy';
