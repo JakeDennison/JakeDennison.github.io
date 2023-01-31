200df1a5a51403cc2b721d1fb9a900a75d3138f4
JSDenintex <jake.dennison@nintex.com>
2023-01-31 16:43:50 +0000

    .

--- plugins/nac-translate/nac-translate-legacy.js
+++ plugins/nac-translate/nac-translate-legacy.js
@@ -86,6 +86,7 @@ export class Placeholder extends LitElement {
   _translate(selectLang) {
     console.log("Translate is calling");
     const select = document.querySelector("#language-select");
+    const selectLang = select.options[select.selectedIndex].value;
     console.log(selectLang);
     if (selectLang === "English") {
       // original translations
@@ -127,17 +128,12 @@ export class Placeholder extends LitElement {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <label for="language-select">Translate Plugin</label>
-    <div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="language-select" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      Language
-    </button>
-    <div class="dropdown-menu" aria-labelledby="language-select">
-      <a class="dropdown-item" href="#">English</a>
-      <a class="dropdown-item" href="#">German</a>
-      <a class="dropdown-item" href="#">Spanish</a>
-      <a class="dropdown-item" href="#">Dutch</a>
-    </div>
-  </div>
+    <select class="form-select" aria-label="English" id="language-select" @change="${this._translate}">
+      <option selected>English</option>
+      <option value="German">German</option>
+      <option value="Spanish">Spanish</option>
+      <option value="Dutch">Dutch</option>
+    </select>
     `;
   }
 
