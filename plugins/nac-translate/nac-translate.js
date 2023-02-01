39d745770ee3d62f7a43a0e5c82722b2cc916a0a
JSDenintex <jake.dennison@nintex.com>
2023-02-01 12:00:37 +0000

    .

--- plugins/nac-translate/nac-translate.js
+++ plugins/nac-translate/nac-translate.js
@@ -61,7 +61,6 @@ export class TranslateMod extends LitElement {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <select class="selectpicker countrypicker" data-countries="AT,BE,BG,HR,CY,CZ,DK,EE,FI,FR,DE,GR,HU,IE,IT,LV,LT,LU,MT,NL,PL,PT,RO,SK,SI,ES,SE" ></select>
     <div class="dropdown">
         <a class="dropdown-toggle" href="#" id="Dropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
             <i class="flag-united-kingdom flag m-0"></i>
