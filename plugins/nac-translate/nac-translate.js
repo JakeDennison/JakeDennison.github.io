3c7d1862424fe9f42c54bf8316192c1dae0a239b
JSDenintex <jake.dennison@nintex.com>
2023-02-01 11:29:29 +0000

    .

--- plugins/nac-translate/nac-translate.js
+++ plugins/nac-translate/nac-translate.js
@@ -58,8 +58,10 @@ export class TranslateMod extends LitElement {
 
   render() {
     return html`
-      <select @change="${this._handleLanguageChange}">
-        <option value="English">English</option>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+      <select class="form-select" aria-label="English" id="language-select" @change="${this._handleLanguageChange}">
+        <option selected>English</option>
         <option value="German">German</option>
         <option value="Spanish">Spanish</option>
         <option value="Dutch">Dutch</option>
