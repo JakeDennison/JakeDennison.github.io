b4c6656fc740549f092b0cad03eda1d3aea0b05f
JSDenintex <jake.dennison@nintex.com>
2023-02-01 14:57:18 +0000

    .

--- plugins/nac-cards/nac-cards.js
+++ plugins/nac-cards/nac-cards.js
@@ -2,10 +2,7 @@ import { html, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit
 
 export default class NacCards extends LitElement {
     static properties = {
-        cardTitle: { type: String },
-        cardText: { type: String },
-        imageUrl: { type: String },
-        link: { type: String },
+        cardsData: { type: Array },
         darkMode: { type: Boolean },
         centerText: { type: Boolean }
     };
@@ -56,30 +53,35 @@ export default class NacCards extends LitElement {
         let cardStyle = this.darkMode ? "text-white bg-dark" : "";
         let centerTextStyle = this.centerText ? "text-center" : "";
         return html`
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-            <div class="row row-cols-1 row-cols-md-3 g-4">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            ${this.cardsData.map(cardData => html`
                 <div class="col">
                     <div class="card ${centerTextStyle} ${cardStyle} mb-3">
-                        <h5 class="card-header">${this.cardTitle}</h5>
-                        <img src="${this.imageUrl}" class="card-img-top p-3" alt=""/>
+                        <h5 class="card-header">${cardData.cardTitle}</h5>
+                        <img src="${cardData.imageUrl}" class="card-img-top p-3" alt=""/>
                         <div class="card-body">
-                            <p class="card-text">${this.cardText}</p>
-                            <a href="${this.link}" class="btn btn-primary">Go!</a>
+                            <p class="card-text">${cardData.cardText}</p>
+                            <a href="${cardData.link}" class="btn btn-primary">Go!</a>
                         </div>
                     </div>
                 </div>
-            </div>
-            `;
+            `)}
+        </div>
+        `;
     }
 
     constructor() {
         super();
-        this.cardText = "This is a card";
-        this.cardTitle = "My Card plugin";
-        this.imageUrl = "https://www.nintex.com/wp-content/themes/portent_nintex/ui/images/nintex_logo.svg";
-        this.link = "https://www.nintex.com/"
         this.darkMode = false;
+        this.centerText = false;
+        this.cardsData = [];
+        fetch('./cards.json')
+            .then(response => response.json())
+            .then(data => {
+                this.cardsData = data;
+            });
     }
 }
 
