42f2825e344775d59baf819d151a5ce4c71352c0
JSDenintex <jake.dennison@nintex.com>
2023-02-08 15:00:32 +0000

    .

--- plugins/nac-translate/nac-translate.js
+++ plugins/nac-translate/nac-translate.js
@@ -52,14 +52,16 @@ export class TranslateMod extends LitElement {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@6.6.6/css/flag-icons.min.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <div class="container row m-0">
-      <select class="form-select m-0 col-md-3 col-sm-12" aria-label="English" id="language-select" @change="${this._handleLanguageChange}">
-        <option selected value="English" data-content='<i class="fi fi-gb"></i>'>English</option>
-        <option value="German" data-content='<i class="fi fi-de"></i>'>Deutsch</option>
-        <option value="French" data-content='<i class="fi fi-fr"></i>'>Français</option>
-        <option value="Spanish" data-content='<i class="fi fi-es"></i>'>Español</option>
-        <option value="Dutch" data-content='<i class="fi fi-nl"></i>'>Nederlands</option>
-      </select>
+    <div class="container">
+      <div class="row">
+        <select class="form-select m-0 col-md-3 col-sm-12" aria-label="English" id="language-select" @change="${this._handleLanguageChange}">
+          <option selected value="English" data-content='<i class="fi fi-gb"></i>'>English</option>
+          <option value="German" data-content='<i class="fi fi-de"></i>'>Deutsch</option>
+          <option value="French" data-content='<i class="fi fi-fr"></i>'>Français</option>
+          <option value="Spanish" data-content='<i class="fi fi-es"></i>'>Español</option>
+          <option value="Dutch" data-content='<i class="fi fi-nl"></i>'>Nederlands</option>
+        </select>
+      </div>
     </div>
     `;
   }
