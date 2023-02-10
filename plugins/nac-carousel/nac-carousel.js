8a1e42530bdc098b546df40e0ab4c0e9760e61c5
JSDenintex <jake.dennison@nintex.com>
2023-02-10 09:49:42 +0000

    .

--- plugins/nac-carousel/nac-carousel.js
+++ plugins/nac-carousel/nac-carousel.js
@@ -54,6 +54,12 @@ class CarouselElement extends LitElement {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         <style>
+            .carousel-item {
+                -webkit-transition: opacity 0.6s ease-in-out;
+                -moz-transition: opacity 0.6s ease-in-out;
+                -o-transition: opacity 0.6s ease-in-out;
+                transition: opacity 0.6s ease-in-out;
+            }
             img {
                 object-fit: contain;
             }
@@ -69,11 +75,11 @@ class CarouselElement extends LitElement {
                     `;
             })}
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev" @click="${this.handlePrev}">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next" @click="${this.handleNext}">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
@@ -81,8 +87,5 @@ class CarouselElement extends LitElement {
         `;
     }
 }
-
-//@click="${this.handlePrev}"
-//@click="${this.handleNext}"
 
 customElements.define('nac-carousel', CarouselElement);
