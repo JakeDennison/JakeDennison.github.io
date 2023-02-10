5f33464755a4e3774e370a077c176867f80c226c
JSDenintex <jake.dennison@nintex.com>
2023-02-10 09:46:45 +0000

    .

--- plugins/nac-carousel/nac-carousel.js
+++ plugins/nac-carousel/nac-carousel.js
@@ -51,15 +51,9 @@ class CarouselElement extends LitElement {
         this.imageList = this.images.split(';').filter(image => image.trim() !== '');;
 
         return html`
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         <style>
-            .carousel-item {
-                -webkit-transition: opacity 0.6s ease-in-out;
-                -moz-transition: opacity 0.6s ease-in-out;
-                -o-transition: opacity 0.6s ease-in-out;
-                transition: opacity 0.6s ease-in-out;
-            }
             img {
                 object-fit: contain;
             }
@@ -75,11 +69,11 @@ class CarouselElement extends LitElement {
                     `;
             })}
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev" @click="${this.handlePrev}">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next" @click="${this.handleNext}">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
@@ -87,5 +81,8 @@ class CarouselElement extends LitElement {
         `;
     }
 }
+
+//@click="${this.handlePrev}"
+//@click="${this.handleNext}"
 
 customElements.define('nac-carousel', CarouselElement);
