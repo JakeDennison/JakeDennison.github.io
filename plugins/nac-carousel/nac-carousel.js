f57cddcb1d20a52278400fc9c33ad6c2069da771
JSDenintex <jake.dennison@nintex.com>
2023-02-10 09:52:38 +0000

    .

--- plugins/nac-carousel/nac-carousel.js
+++ plugins/nac-carousel/nac-carousel.js
@@ -75,11 +75,11 @@ class CarouselElement extends LitElement {
                     `;
             })}
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev" @click="${this.handlePrev}">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next" @click="${this.handleNext}">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
