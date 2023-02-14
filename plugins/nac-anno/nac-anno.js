6779b5f71a46d824d424e33952b8b9fff849d8e4
JSDenintex <jake.dennison@nintex.com>
2023-02-14 15:00:49 +0000

    .

---
+++ plugins/nac-anno/nac-anno.js
@@ -0,0 +1,51 @@
+import { html, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+import { Marker } from 'https://cdn.jsdelivr.net/npm/markerjs2@1.1.0/dist/marker.min.js';
+
+class AnnoElement extends LitElement {
+    static getMetaConfig() {
+        // plugin contract information
+        return {
+            controlName: 'nac-anno',
+            fallbackDisableSubmit: false,
+            description: 'Display images in a carousel',
+            iconUrl: "image",
+            groupName: 'Visual Data',
+            version: '1.3',
+            properties: {
+                image: {
+                    type: 'string',
+                    title: 'Image',
+                    description: 'Please enter the URL you want to annotate'
+                },
+            },
+            standardProperties: {
+                fieldLabel: true,
+                readOnly: true,
+                required: true,
+                description: true,
+            }
+        };
+    }
+    constructor() {
+        super();
+        this.image = '';
+    }
+
+    firstUpdated() {
+        super.firstUpdated();
+        const markerContainer = this.shadowRoot.querySelector('#marker-container');
+        this.marker = new Marker(markerContainer);
+    }
+
+    render() {
+        if (!this.image) {
+            return html`<p>No image found</p>`;
+        }
+        return html`
+            <div id="marker-container"></div>
+            <img src="${this.image}" onclick="${() => this.marker.annotate()}" />
+        `;
+    }
+}
+
+customElements.define('nac-anno', AnnoElement);
