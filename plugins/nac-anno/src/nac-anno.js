8cbf2863a1467f345865303b9a6bab068a1c29b4
JSDenintex <jake.dennison@nintex.com>
2023-02-15 15:01:56 +0000

    .

--- plugins/nac-anno/src/nac-anno.js
+++ plugins/nac-anno/src/nac-anno.js
@@ -29,24 +29,28 @@ class AnnoElement extends LitElement {
     this.markerArea = '';
   }
 
-  showMarkerArea(target) {
-    const markerArea = new markerjs2.MarkerArea(target);
-    console.log('markerArea:', markerArea);
-    markerArea.addEventListener('done', (event) => {
-      console.log('dataUrl:', event.dataUrl);
-      this.src = event.dataUrl;
-    });
-    markerArea.show();
-  }
-
   render() {
     if (!this.src) {
       return html`<p>No image found</p>`;
     }
     return html`
-          <img src="${this.src}" style="max-width: 600px;" @click="${(e) => this.showMarkerArea(e.target)}" />
+          <img id="personimg" src="${this.src}"/>
         `;
   }
 }
 
 customElements.define('nac-anno', AnnoElement);
+
+// create an instance of MarkerArea and pass the target image reference as a parameter
+let markerArea = new markerjs2.MarkerArea(document.getElementById('personimg'));
+
+// register an event listener for when user clicks OK/save in the marker.js UI
+markerArea.addEventListener('render', event => {
+  // we are setting the markup result to replace our original image on the page
+  // but you can set a different image or upload it to your server
+  document.getElementById('personimg').src = event.dataUrl;
+});
+
+// finally, call the show() method and marker.js UI opens
+markerArea.show();
+
