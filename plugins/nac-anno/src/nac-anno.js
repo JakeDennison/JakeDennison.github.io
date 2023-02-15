104e7bfa5faf918153ad6331478dd375c93841ad
JSDenintex <jake.dennison@nintex.com>
2023-02-15 14:18:08 +0000

    ./

--- plugins/nac-anno/src/nac-anno.js
+++ plugins/nac-anno/src/nac-anno.js
@@ -1,52 +1,60 @@
-import { html, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+import { html, LitElement } from 'lit';
+import * as markerjs2 from 'markerjs2';
 
 class AnnoElement extends LitElement {
-    static getMetaConfig() {
-        // plugin contract information
-        return {
-            controlName: 'nac-anno',
-            fallbackDisableSubmit: false,
-            description: 'Display images in a carousel',
-            iconUrl: "image",
-            groupName: 'Visual Data',
-            version: '1.3',
-            properties: {
-                image: {
-                    type: 'string',
-                    title: 'Image',
-                    description: 'Please enter the URL you want to annotate'
-                },
-            },
-            standardProperties: {
-                fieldLabel: true,
-                readOnly: true,
-                required: true,
-                description: true,
-            }
-        };
-    }
-
-    constructor() {
-        super();
-        this.image = '';
+  static getMetaConfig() {
+    // plugin contract information
+    return {
+      controlName: 'nac-anno',
+      fallbackDisableSubmit: false,
+      description: 'Display images in a carousel',
+      iconUrl: "image",
+      groupName: 'Visual Data',
+      version: '1.3',
+      properties: {
+        image: {
+          type: 'string',
+          title: 'Image',
+          description: 'Please enter the URL you want to annotate'
+        },
+      },
+      standardProperties: {
+        fieldLabel: true,
+        readOnly: true,
+        required: true,
+        description: true,
       }
+    };
+  }
 
-      render() {
-        if (!this.image) {
-            return html`<p>No image found</p>`;
-          }
-        return html`
+  constructor() {
+    super();
+    this.image = '';
+  }
+
+  firstUpdated() {
+    super.firstUpdated();
+    const imgElement = this.shadowRoot.querySelector('img');
+    this.markerArea = new markerjs2.MarkerArea(imgElement, {});
+    this.markerArea.on('done', (dataUrl) => {
+      imgElement.src = dataUrl;
+    });
+  }
+
+  render() {
+    if (!this.image) {
+      return html`<p>No image found</p>`;
+    }
+    return html`
         
           <style>
             :host {
               display: block;
             }
           </style>
-          <img id="annoimg" src="${this.image}"/>
+          <img src="${this.image}" @click="${() => this.markerArea.showMarkerArea()}" />
         `;
-      }
-    }
-    
-    customElements.define('nac-anno', AnnoElement);
+  }
+}
 
-    
+customElements.define('nac-anno', AnnoElement);
