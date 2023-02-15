01043b3bb64ca0bef86f8dd6afa42f8d18badbf3
JSDenintex <jake.dennison@nintex.com>
2023-02-15 15:26:17 +0000

    .

--- plugins/nac-anno/src/nac-anno.js
+++ plugins/nac-anno/src/nac-anno.js
@@ -12,6 +12,7 @@ class AnnoElement extends LitElement {
       groupName: 'Visual Data',
       version: '1.3',
       src: {type: String},
+      image: {type: String},
       properties: {
       },
       standardProperties: {
@@ -26,7 +27,14 @@ class AnnoElement extends LitElement {
   constructor() {
     super();
     this.src = 'https://jsdenintex.github.io/plugins/nac-anno/dist/img/person.png';
-    this.markerArea = '';
+    this.showMarkerArea = this.showMarkerArea.bind(this);
+  }
+
+  showMarkerArea(event) {
+    const target = event.target;
+    const markerArea = new markerjs2.MarkerArea(target);
+    markerArea.addEventListener('render', (event) => (target.src = event.dataUrl));
+    markerArea.show();
   }
 
   render() {
@@ -34,23 +42,9 @@ class AnnoElement extends LitElement {
       return html`<p>No image found</p>`;
     }
     return html`
-          <img id="personimg" src="${this.src}"/>
+          <img id="personimg" src="${this.src}" @click="${this.showMarkerArea}"/>
         `;
   }
 }
 
 customElements.define('nac-anno', AnnoElement);
-
-// create an instance of MarkerArea and pass the target image reference as a parameter
-let markerArea = new markerjs2.MarkerArea(document.getElementById('personimg'));
-
-// register an event listener for when user clicks OK/save in the marker.js UI
-markerArea.addEventListener('render', event => {
-  // we are setting the markup result to replace our original image on the page
-  // but you can set a different image or upload it to your server
-  document.getElementById('personimg').src = event.dataUrl;
-});
-
-// finally, call the show() method and marker.js UI opens
-markerArea.show();
-
