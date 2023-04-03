b3a77481c31c4a93b38983c4bbe2f0ae69a6568a
JSDenintex <jake.dennison@nintex.com>
2023-04-03 10:59:28 +0100

    .

--- plugins/neo-ar/src/neo-ar.js
+++ plugins/neo-ar/src/neo-ar.js
@@ -1,4 +1,5 @@
 import { LitElement, html } from 'lit-element';
+import * as BABYLON from "@babylonjs/core/Legacy/legacy";
 
 class AnnoElement extends LitElement {
   static getMetaConfig() {
@@ -27,8 +28,10 @@ class AnnoElement extends LitElement {
 
   constructor() {
     super();
-    this.src = '';
-    this.viewer = null;
+    this.src = 'https://jsdenintex.github.io/plugins/neo-ar/dist/assets/valve.gltf';
+    this.canvas = null;
+    this.engine = null;
+    this.scene = null;
     this.model = null;
     this.ready = false;
     this.updateSize = this.updateSize.bind(this);
@@ -36,46 +39,38 @@ class AnnoElement extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
-    const script = document.createElement('script');
-    script.setAttribute('src', 'https://cesium.com/downloads/cesiumjs/releases/1.78/Build/Cesium/Cesium.js');
-    script.onload = () => {
-      this.viewer = new Cesium.Viewer(this.shadowRoot, {
-        shouldAnimate: true,
-        animation: false,
-        timeline: false,
-        fullscreenButton: false,
-        geocoder: false,
-        homeButton: false,
-        sceneModePicker: false,
-        navigationHelpButton: false,
-        baseLayerPicker: false
-      });
-      this.viewer.scene.globe.show = false;
-      this.viewer.camera.setView({
-        destination: new Cesium.Cartesian3.fromDegrees(0, 0, 2000),
-        orientation: {
-          heading: 0,
-          pitch: -Cesium.Math.PI_OVER_TWO,
-          roll: 0
-        }
-      });
-      this.loadModel();
-      this.updateSize();
-      window.addEventListener('resize', this.updateSize);
-    };
-    document.body.appendChild(script);
+    this.canvas = document.createElement('canvas');
+    this.shadowRoot.appendChild(this.canvas);
+    this.engine = new BABYLON.Engine(this.canvas, true);
+    this.scene = new BABYLON.Scene(this.engine);
+    this.scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
+    this.scene.createDefaultCameraOrLight(true, true, true);
+    this.loadModel();
+    this.updateSize();
+    window.addEventListener('resize', this.updateSize);
+    this.engine.runRenderLoop(() => {
+      if (this.model) {
+        this.model.rotation.y += 0.01;
+      }
+      this.scene.render();
+    });
   }
 
   disconnectedCallback() {
     super.disconnectedCallback();
     window.removeEventListener('resize', this.updateSize);
     if (this.model) {
-      this.viewer.entities.remove(this.model);
+      this.model.dispose();
       this.model = null;
     }
-    if (this.viewer) {
-      this.viewer.destroy();
-      this.viewer = null;
+    if (this.scene) {
+      this.scene.dispose();
+      this.scene = null;
+    }
+    if (this.engine) {
+      this.engine.stopRenderLoop();
+      this.engine.dispose();
+      this.engine = null;
     }
   }
 
@@ -84,29 +79,31 @@ class AnnoElement extends LitElement {
       console.error('No model source provided');
       return;
     }
-    const tileset = this.viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
-      url: this.src,
-      maximumNumberOfLoadedTiles: 1000
-    }));
-    tileset.readyPromise.then(() => {
+    const loader = new BABYLON.AssetsManager(this.scene);
+    const task = loader.addMeshTask('model', '', './', this.src);
+    task.onSuccess = task => {
+      this.model = task.loadedMeshes[0];
+      this.model.scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
+      this.model.position = new BABYLON.Vector3(0, 0, -1);
       this.ready = true;
-    }).catch(error => {
-      console.error(`Error loading model from ${this.src}:`, error);
+    };
+    task.onError = (task, message, exception) => {
+      console.error(`Error loading model from ${this.src}:`, message, exception);
       this.dispatchEvent(
         new CustomEvent('error', {
           detail: {
-            message: `Error loading model from ${this.src}: ${error.message}`,
+            message: `Error loading model from ${this.src}: ${message}`,
           },
         })
       );
-    });
-    this.model = tileset;
+    };
+    loader.load();
   }
 
   updateSize() {
     const { width, height } = this.getBoundingClientRect();
-    this.viewer.canvas.width = width;
-    this.viewer.canvas.height = height;
+    this.canvas.width = width;
+    this.canvas.height = height;
   }
 
   render() {
