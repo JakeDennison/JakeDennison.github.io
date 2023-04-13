c6822fde6ecc7d1952e8ab8fd6c5765e14e21b92
JSDenintex <jake.dennison@nintex.com>
2023-04-13 12:40:14 +0100

    .

--- plugins/neo-table/neo-table.js
+++ plugins/neo-table/neo-table.js
@@ -54,7 +54,7 @@ export class MyTable extends LitElement {
     return html`
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-      <div class="table-responsive">
+      <div class="table-responsive overflow-auto">
       <table class="table table-striped">
         <thead>
           <tr>
