4767b17e4ac11a4312122fb8f7f2fa8265e3d1a5
JSDenintex <jake.dennison@nintex.com>
2023-04-13 12:22:49 +0100

    added unicode translation

--- plugins/neo-table/neo-table.js
+++ plugins/neo-table/neo-table.js
@@ -37,19 +37,20 @@ export class MyTable extends LitElement {
   render() {
     if (!this.dataobject) {
       return html`
-      <p>No Data Loaded</p>
+        <p>No Data Loaded</p>
       `;
     }
-
-    const data = JSON.parse(this.dataobject);
+  
+    const unicodeRegex = /\\u([\dA-Fa-f]{4})/g; // regular expression for matching all Unicode characters
+    const data = JSON.parse(this.dataobject.replace(unicodeRegex, (match, p1) => String.fromCharCode(parseInt(p1, 16)))); // Replace all Unicode characters with actual characters
     const rows = data.map(row => html`
       <tr>
         ${Object.values(row).map(cell => html`<td>${cell}</td>`)}
       </tr>
     `);
-
+  
     const headers = Object.keys(data[0]).map(header => html`<th>${header}</th>`);
-
+  
     return html`
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
