43a3645dc5ef7e5bfb43a4267ea2b2c63bc9e5be
JSDenintex <jake.dennison@nintex.com>
2023-04-13 12:53:01 +0100

    .

--- plugins/neo-table/neo-table.js
+++ plugins/neo-table/neo-table.js
@@ -1,4 +1,4 @@
-import {css, html, LitElement, styleMap} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+import {html, LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
 export class MyTable extends LitElement {
   static getMetaConfig() {
