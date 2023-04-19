cb7624e1c7a10cc321e93abff5f7ec87f49e717b
JSDenintex <jake.dennison@nintex.com>
2023-04-19 15:00:17 +0100

    .

--- plugins/neo-carousel/src/neo-carousel.js
+++ plugins/neo-carousel/src/neo-carousel.js
@@ -1,7 +1,7 @@
 import { LitElement, html, css } from 'lit';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './css/carousel.css';
 import './css/neo-carousel.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 class CarouselElement extends LitElement {
   static getMetaConfig() {
@@ -35,26 +35,44 @@ class CarouselElement extends LitElement {
   }
 
   firstUpdated() {
-    // Initialize the carousel using the "carousel" class
-    const carousel = this.shadowRoot.querySelector('.carousel');
-    $(carousel).carousel();
+    // Initialize the carousel using the "Carousel" class provided by Bootstrap
+    const carouselElement = this.shadowRoot.querySelector('.carousel');
+    const carousel = new bootstrap.Carousel(carouselElement, {
+      interval: 5000, // Set the interval time for the carousel to 5 seconds
+      keyboard: true, // Enable keyboard navigation for the carousel
+      pause: 'hover', // Pause the carousel on mouse hover
+      wrap: true // Enable wrapping of the carousel items
+    });
   }
 
   render() {
     if (!this.images) {
       return html`<p>No images found</p>`;
     }
-  
+
     const imageUrls = this.images.split(';');
-  
+
     return html`
-      <div class="carousel">
-        ${imageUrls.map(url => html`
-          <img src="${url}" />
-        `)}
+      <div class="carousel slide">
+        <div class="carousel-inner">
+          ${imageUrls.map((url, index) => html`
+            <div class="carousel-item ${index === 0 ? 'active' : ''}">
+              <img src="${url}" class="d-block w-100" alt="Image ${index + 1}">
+            </div>
+          `)}
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target=".carousel" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target=".carousel" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
     `;
   }
 }
+
 
 customElements.define('neo-carousel', CarouselElement);
