950c38dcc02445395033ee95087f1f21d4988b2b
JSDenintex <jake.dennison@nintex.com>
2023-04-19 15:15:38 +0100

    .

--- plugins/neo-carousel/src/neo-carousel.js
+++ plugins/neo-carousel/src/neo-carousel.js
@@ -1,77 +1,100 @@
-import { LitElement, html, css } from 'lit';
-import 'bootstrap/js/dist/carousel.js';
-import './css/carousel.css';
-import './css/neo-carousel.css';
+import { html, LitElement } from 'lit';
 
 class CarouselElement extends LitElement {
-  static getMetaConfig() {
-    // plugin contract information
-    return {
-      controlName: 'neo-carousel',
-      fallbackDisableSubmit: false,
-      description: 'Display images in a carousel',
-      iconUrl: 'image',
-      groupName: 'Visual Data',
-      version: '1.3',
-      properties: {
-        images: {
-          type: 'string',
-          title: 'Images',
-          description: 'Please list image URLs semi-colon (;) separated'
-        },
-      },
-      standardProperties: {
-        fieldLabel: true,
-        readOnly: true,
-        required: true,
-        description: true,
+    static getMetaConfig() {
+        // plugin contract information
+        return {
+          controlName: 'neo-carousel',
+          fallbackDisableSubmit: false,
+          description: 'Display images in a carousel',
+          iconUrl: "image",
+          groupName: 'Visual Data',
+          version: '1.3',
+          properties: {
+            images: {
+              type: 'string',
+              title: 'Images',
+              description: 'Please list image URLs semi-colon (;) separated'
+            },
+          },
+          standardProperties: {
+            fieldLabel: true,
+            readOnly: true,
+            required: true,
+            description: true,
+          }
+        };
       }
-    };
-  }
-
-  constructor() {
-    super();
-    this.images = '';
-  }
-
-  firstUpdated() {
-    // Initialize the carousel using the "Carousel" class provided by Bootstrap
-    const carouselElement = this.shadowRoot.querySelector('.carousel');
-    const carousel = new bootstrap.Carousel(carouselElement, {
-      interval: 5000, // Set the interval time for the carousel to 5 seconds
-      keyboard: true, // Enable keyboard navigation for the carousel
-      pause: 'hover', // Pause the carousel on mouse hover
-      wrap: true // Enable wrapping of the carousel items
-    });
-  }
-
-  render() {
-    if (!this.images) {
-      return html`<p>No images found</p>`;
-    }
-
-    const imageUrls = this.images.split(';');
-
-    return html`
-      <div class="carousel slide">
-        <div class="carousel-inner">
-          ${imageUrls.map((url, index) => html`
-            <div class="carousel-item ${index === 0 ? 'active' : ''}">
-              <img src="${url}" class="d-block w-100" alt="Image ${index + 1}">
+      constructor() {
+        super();
+        this.images = '';
+        this.index = 0;
+      }
+    
+      handleNext() {
+        if (this.index === this.imageList.length - 1) {
+          // Set index to 0 and wait for transition
+          this.index = 0;
+          setTimeout(() => this.requestUpdate(), 50);
+        } else {
+          this.index++;
+          this.requestUpdate();
+        }
+      }
+      
+      handlePrev() {
+        if (this.index === 0) {
+          // Set index to last image and wait for transition
+          this.index = this.imageList.length - 1;
+          setTimeout(() => this.requestUpdate(), 50);
+        } else {
+          this.index--;
+          this.requestUpdate();
+        }
+      }
+          
+      updated() {
+        const prevIndex = this.index === 0 ? this.imageList.length - 1 : this.index - 1;
+        const nextIndex = this.index === this.imageList.length - 1 ? 0 : this.index + 1;
+        const prevItem = this.shadowRoot.querySelector(`#item-${prevIndex}`);
+        const activeItem = this.shadowRoot.querySelector(`#item-${this.index}`);
+        const nextItem = this.shadowRoot.querySelector(`#item-${nextIndex}`);
+        prevItem.style.transform = 'translateX(-100%)';
+        activeItem.style.transform = 'translateX(0)';
+        nextItem.style.transform = 'translateX(100%)';
+      }
+    
+      render() {
+        if (!this.images) {
+          return html`<p>No images found</p>`;
+        }
+      
+        this.imageList = this.images.split(';').filter(image => image.trim() !== '');
+        return html`
+          <link href="https://jsdenintex.github.io/plugins/neo-carousel/neo-carousel.css" rel="stylesheet">
+          <div class="carousel">
+            <div class="carousel-inner">
+              ${this.imageList.map((image, i) => {
+                const isActive = i === this.index;
+                const isPrev = i === (this.index === 0 ? this.imageList.length - 1 : this.index - 1);
+                const isNext = i === (this.index === this.imageList.length - 1 ? 0 : this.index + 1);
+                return html`
+                  <div
+                    id="item-${i}"
+                    class="carousel-item ${isActive ? 'active' : ''} ${isPrev || isNext ? 'visible' : ''}"
+                  >
+                    <img src="${image}" alt="">
+                  </div>
+                `;
+              })}
             </div>
-          `)}
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target=".carousel" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target=".carousel" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
-    `;
+            <button class="carousel-control carousel-control-prev" @click="${this.handlePrev}">
+            </button>
+            <button class="carousel-control carousel-control-next" @click="${this.handleNext}">
+            </button>
+          </div>
+        `;
+    }
   }
-}
-
+    
 customElements.define('neo-carousel', CarouselElement);
