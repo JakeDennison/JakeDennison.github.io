8d22619b08d88fa873dec2f3f17bad6c61d9dc08
JSDenintex <jake.dennison@nintex.com>
2023-04-19 11:21:22 +0100

    .

--- plugins/neo-table/neo-table.js
+++ plugins/neo-table/neo-table.js
@@ -36,73 +36,72 @@ export class MyTable extends LitElement {
 
   render() {
     let data;
-    console.log(this.dataobject);
+    const unicodeRegex = /_x([0-9A-F]{4})_/g;
+
     try {
-      // Convert Unicode escape sequences if present
-      data = JSON.parse(this.dataobject.replace(/([\u007f-\uffff])/g, function(chr) {
-        return "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).substr(-4);
-      }));      
-    } catch (e) {
-      // If parsing as JSON fails, assume it's XML
-      console.log("XML detected");
-      const xmlString = this.dataobject.replace(/&quot;/g, '"').replace(/_x([\dA-F]{4})_/gi, (match, p1) => String.fromCharCode(parseInt(p1, 16)));
-      const parser = new DOMParser();
-      const xmlDocument = parser.parseFromString(xmlString, 'text/xml');
-      const items = xmlDocument.getElementsByTagName('Item');
-      data = [];
-  
-      for (let i = 0; i < items.length; i++) {
-        const row = {};
-        const item = items[i];
-        const fields = item.children;
-  
-        for (let j = 0; j < fields.length; j++) {
-          const field = fields[j];
-          const fieldName = field.nodeName;
-          let fieldValue = field.textContent;
-  
-          // Convert Unicode escape sequences if present
-          fieldValue = fieldValue.replace(/_x([\dA-F]{4})_/gi, (match, p1) => String.fromCharCode(parseInt(p1, 16)));
-  
-          row[fieldName] = fieldValue;
-        }
-  
-        data.push(row);
+      data = JSON.parse(this.dataobject.replace(unicodeRegex, (match, p1) => String.fromCharCode(parseInt(p1, 16))));
+    }
+    catch (e) {
+    // If parsing as JSON fails, assume it's XML
+    console.log("XML detected");
+    const xmlString = this.dataobject.replace(/&quot;/g, '"').replace(/_x([\dA-F]{4})_/gi, (match, p1) => String.fromCharCode(parseInt(p1, 16)));
+    const parser = new DOMParser();
+    const xmlDocument = parser.parseFromString(xmlString, 'text/xml');
+    const items = xmlDocument.getElementsByTagName('Item');
+    data = [];
+
+    for (let i = 0; i < items.length; i++) {
+      const row = {};
+      const item = items[i];
+      const fields = item.children;
+
+      for (let j = 0; j < fields.length; j++) {
+        const field = fields[j];
+        const fieldName = field.nodeName;
+        let fieldValue = field.textContent;
+
+        // Convert Unicode escape sequences if present
+        fieldValue = fieldValue.replace(/_x([\dA-F]{4})_/gi, (match, p1) => String.fromCharCode(parseInt(p1, 16)));
+
+        row[fieldName] = fieldValue;
       }
-  
-      console.log('XML converted to JSON:', data);
+
+      data.push(row);
     }
-  
-    if (!data || data.length === 0) {
-      return html`
-        <p>No Data Found</p>
-      `;
-    }
-  
+
+    console.log('XML converted to JSON:', data);
+  }
+    
+  if (!data || data.length === 0) {
+    return html`
+      <p>No Data Found</p>
+    `;
+  }
+    
     const rows = data.map(row => html`
       <tr>
         ${Object.values(row).map(cell => html`<td class="text-nowrap">${cell}</td>`)}
       </tr>
     `);
   
-    const headers = Object.keys(data[0]).map(header => html`<th class="text-nowrap">${header}</th>`);
+    const headers = Object.keys(data[0]).map(header => html`<th class="text-nowrap">${header}</th>`); // add class to th elements
   
     return html`
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
       <div class="table-responsive-md overflow-auto">
-        <table class="table table-striped">
-          <thead>
-            <tr>
-              ${headers}
-            </tr>
-          </thead>
-          <tbody>
-            ${rows}
-          </tbody>
-        </table>
-      </div>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            ${headers}
+          </tr>
+        </thead>
+        <tbody>
+          ${rows}
+        </tbody>
+      </table>
+    </div>
     `;
-  }  
+  }
 }
 
 customElements.define('neo-table', MyTable);
