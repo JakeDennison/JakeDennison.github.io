a21d27a6bd297758180cc03f25177ffd6d3dcdc3
JSDenintex <jake.dennison@nintex.com>
2023-04-28 13:31:07 +0100

    added commit for break

--- plugins/neo-unit/src/neo-unit.js
+++ plugins/neo-unit/src/neo-unit.js
@@ -1,146 +1,80 @@
 import { LitElement, html, css } from 'lit';
-import { WebGLRenderer, PerspectiveCamera, Scene, Color, AmbientLight, DirectionalLight } from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 
-class templateElement extends LitElement {
+class unitElement extends LitElement {
   static getMetaConfig() {
     // plugin contract information
     return {
       controlName: 'neo-unit',
       fallbackDisableSubmit: false,
-      description: 'Display AR images',
+      description: 'Control for diplaying units of measurement',
       iconUrl: "image",
       groupName: 'Visual Data',
       version: '1.0',
       properties: {
-        src: {
-          type: 'string',
-          title: '3D Object source',
-          description: 'Please provide a link to the 3d object (.glt or .gltf)'
+        unitvalue: {
+          type: 'number',
+          title: 'Unit value',
+          description: 'Decimal unit value',
+          isValueField: true,
+        },
+        decimalplaces: {
+          type: 'integer',
+          title: 'Decimal place',
+          description: 'enter -1 for none, 1 for .0, 2 for .01 etc.',
+          isValueField: true,
         },
       },
       standardProperties: {
         fieldLabel: true,
         description: true,
-      }
+      },
+      events: ["ntx-value-change"],
     };
   }
 
   static get styles() {
-    return css`
-      :host {
-        display: block;
-      }
-  
-      .container {
-        width: 100%;
-        height: 600px;
-      }
-    `;
-  }
-  
-  static get properties() {
-    return {
-      modelLoaded: { type: Boolean },
-      error: { type: String },
-      container: { type: Object },
-    };
+    return [
+      css`
+        @import url('./neo-unit.css');
+      `,
+    ];
   }
 
   constructor() {
     super();
-    this.src = '';
-    this.container = document.createElement('div');
   }
 
-  firstUpdated() {
-    super.firstUpdated();
-    this.initScene();
-    window.dispatchEvent(new Event('resize'));
-  }
-  
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    this.disposeScene();
-  }
-
-  initScene() {
-    const renderer = new WebGLRenderer({ antialias: true });
-    renderer.setSize(this.clientWidth, 600);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.domElement.style.width = '100%';
-    this.container.appendChild(renderer.domElement);
-  
-    const camera = new PerspectiveCamera(75, this.clientWidth / 600, 0.1, 1000);
-    camera.position.set(0, 0, 2);
-    camera.rotation.x = -Math.PI / 6; // set camera pitch to look down slightly
-  
-    const controls = new OrbitControls(camera, renderer.domElement);
-  
-    const scene = new Scene();
-    scene.background = new Color(0xffffff);
-  
-    const ambientLight = new AmbientLight(0xffffff, 0.75);
-    scene.add(ambientLight);
-  
-    const directionalLight = new DirectionalLight(0xffffff, 0.75);
-    directionalLight.position.set(1, 1, 1);
-    scene.add(directionalLight);
-  
-    const loader = new GLTFLoader();
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath('https://jsdenintex.github.io/plugins/neo-unit/dist/draco/js/');
-    loader.setDRACOLoader(dracoLoader);
-    loader.load(this.src, (gltf) => {
-      scene.add(gltf.scene);
-      this.modelLoaded = true;
-    }, undefined, (error) => {
-      console.error('An error occurred:', error);
-      this.error = 'Failed to load the model';
-    });
-  
-    const resize = () => {
-      const { clientWidth } = this;
-      camera.aspect = clientWidth / 600;
-      camera.updateProjectionMatrix();
-      renderer.setSize(clientWidth, 600);
+  onChange(e) {
+    const args = {
+        bubbles: true,
+        cancelable: false,
+        composed: true,
+        detail: e.target.value,
     };
-  
-    window.addEventListener('resize', resize);
-  
-    const animate = () => {
-      requestAnimationFrame(animate);
-      renderer.render(scene, camera);
-    };
-  
-    animate();
-  }
-  
-  
-  disposeScene() {
-    // Dispose of the Three.js resources here when the component is disconnected from the DOM
-  }
+    const event = new CustomEvent('ntx-value-change', args);
+    this.dispatchEvent(event);
+}
 
   render() {
-    if (!this.src) {
-      return html`<p>No model source provided</p>`;
-    }
-    if (!this.modelLoaded) {
-      return html`<p>Loading model...</p>`;
-    }
-    if (this.error) {
-      return html`<p>${this.error}</p>`;
-    }
     return html`
-      <div class="container" id="model-container">
-        ${this.container}
-        <slot></slot>
+      <div class="neo-unit-control">
+        <div class="input-unit-group-append">
+          <button type="button" class="neo-btn-input-icon neo-unit-btn">
+            <span>${this.unittype}</span>
+          </button>
+        </div>
+        <div class="nx-zinc-control-input input-group">
+          <ntx-simple-number>
+            <input type="text" 
+              data-simple-control="true"
+              class="form-control nx-theme-input-1 ng-untouched ng-pristine ng-valid" 
+              decimalplaces=${this.decimalplaces}
+            >
+          </ntx-simple-number>
+        </div>
       </div>
-  `;
+    `;
   }
 }
 
-customElements.define('neo-unit', templateElement);
+customElements.define('neo-unit', unitElement);
