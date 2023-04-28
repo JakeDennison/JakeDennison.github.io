89fb11110f6655ed5ab7e76eb5cd632d2a94ec83
JSDenintex <jake.dennison@nintex.com>
2023-04-28 12:40:10 +0100

    added neo unit

---
+++ plugins/neo-unit/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/neo-unit.js',
+  mode: 'production',
+  output: {
+    filename: 'neo-unit.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
