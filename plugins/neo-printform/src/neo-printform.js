6691617a0e6ad5020797b60f7b47d2e035926a25
JSDenintex <jake.dennison@nintex.com>
2023-05-15 15:14:45 +0100

    .

--- plugins/neo-printform/src/neo-printform.js
+++ plugins/neo-printform/src/neo-printform.js
@@ -17,27 +17,40 @@ class templateElement extends LitElement {
     };
   }
 
-  printButtonClicked() {
-    document.querySelectorAll('.d-none').forEach(element => element.classList.remove('d-none'));
-    window.print();
+  static get styles() {
+    return css`
+      .floating-bar {
+        position: sticky;
+        top: 0;
+        z-index: 9999;
+        background-color: grey;
+        padding: 10px;
+      }
+    `;
   }
-  
+
   constructor() {
     super();
   }
 
   render() {
     return html`
-      <div id="grey-bar" style="background-color: grey; padding: 10px;">
+      <div id="floating-bar" class="floating-bar">
         <button @click="${this.printButtonClicked}">
           <img src="${this.iconUrl}" alt="Print Icon" width="20" height="20">
         </button>
       </div>
-      <div id="nx-form-container">
-        <!-- Existing content of the nx-form-container -->
-      </div>
+      <ntx-form-runtime>
+        <!-- Existing content of the ntx-form-runtime -->
+      </ntx-form-runtime>
     `;
   }
+
+  printButtonClicked() {
+    document.querySelectorAll('.d-none').forEach(element => element.classList.remove('d-none'));
+    window.print();
+  }
+  
 }
 
 customElements.define('neo-printform', templateElement);
