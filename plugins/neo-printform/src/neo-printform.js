a346fc9d6a47fc146c48bf86712daf3877836553
JSDenintex <jake.dennison@nintex.com>
2023-05-16 09:51:44 +0100

    .

--- plugins/neo-printform/src/neo-printform.js
+++ plugins/neo-printform/src/neo-printform.js
@@ -1,6 +1,11 @@
 import { LitElement, html, css } from 'lit';
 
 class templateElement extends LitElement {
+  static get properties() {
+    return {
+      showButton: { type: Boolean },
+    };
+  }
 
   static getMetaConfig() {
     // plugin contract information
@@ -20,12 +25,9 @@ class templateElement extends LitElement {
 
   static get styles() {
     return css`
-      :host {
-        display: block;
-      }
       .print-btn {
         display: inline-block;
-        font-family: Open Sans,Helvetica,Arial,sans-serif;
+        font-family: Open Sans, Helvetica, Arial, sans-serif;
         font-weight: 400;
         color: #161718;
         text-align: center;
@@ -33,13 +35,24 @@ class templateElement extends LitElement {
         -webkit-user-select: none;
         user-select: none;
         border: 1px solid;
-        padding: .525rem .75rem;
-        font-size: .875rem;
+        padding: 0.525rem 0.75rem;
+        font-size: 0.875rem;
         line-height: 1;
         border-radius: 4px;
-        transition: all .2s ease-in-out;
-        margin-left:10px;
-    }
+        transition: all 0.2s ease-in-out;
+        margin-left: 10px;
+      }
+
+      .print-btn.hide-on-print {
+        display: none;
+      }
+
+      /* Print styles */
+      @media print {
+        .print-btn {
+          display: none;
+        }
+      }
     `;
   }
 
@@ -56,21 +69,32 @@ class templateElement extends LitElement {
         element.classList.toggle('d-print-block');
       });
     });
+
+    this.dispatchEvent(
+      new CustomEvent('print-button-clicked', { bubbles: true, composed: true })
+    );
   }
 
   constructor() {
     super();
+    this.showButton = true; // Initialize the button visibility to true
   }
 
   render() {
     return html`
-      <slot></slot>
-      <div class="floating-bar">
-        <button class='print-btn' @click="${this.handlePrintButtonClicked}">
-          <img class="print-icon" src="https://jsdenintex.github.io/plugins/neo-printform/dist/printing-bl.svg" alt="Print Icon" width="20" height="20">
-          <span class="print-text">Print</span>
-        </button>
-      </div>
+      <button
+        class=${this.showButton ? 'print-btn' : 'print-btn hide-on-print'}
+        @click=${this.handlePrintButtonClicked}
+      >
+        <img
+          class="print-icon"
+          src="https://jsdenintex.github.io/plugins/neo-printform/dist/printing-bl.svg"
+          alt="Print Icon"
+          width="20"
+          height="20"
+        />
+        <span class="print-text">Print</span>
+      </button>
     `;
   }
 }
