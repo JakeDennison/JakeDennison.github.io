afd6c2699d12520d0c9100748f0ee5f5178b2a1e
JSDenintex <jake.dennison@nintex.com>
2023-07-03 11:05:33 +0100

    changed to tabulator

--- plugins/neo-listviewer/src/neo-listviewer.js
+++ plugins/neo-listviewer/src/neo-listviewer.js
@@ -1,4 +1,6 @@
 import { LitElement, html, css } from 'lit';
+import Tabulator from 'tabulator-tables'; // Import Tabulator library
+import 'tabulator-tables/dist/css/tabulator.min.css'; // Import Tabulator CSS
 
 class listviewElement extends LitElement {
   static getMetaConfig() {
@@ -54,15 +56,13 @@ class listviewElement extends LitElement {
     super();
     this.dataobject = '';
     this.pageItemLimit = 5;
-    this.currentPage = 1;
-    this.sortedDataobject = ''; // And this line
   }
 
   parseDataObject() {
     let data;
   
     try {
-      data = JSON.parse(this.sortedDataobject || this.dataobject);
+      data = JSON.parse(this.dataobject);
       data = this.replaceUnicodeRegex(data);
     } catch (e) {
       console.error(e);
@@ -72,128 +72,38 @@ class listviewElement extends LitElement {
     return data;
   }
   
-
 replaceUnicodeRegex(input) {
   const unicodeRegex = /_x([0-9A-F]{4})_/g;
   return JSON.parse(JSON.stringify(input).replace(unicodeRegex, (match, p1) => String.fromCharCode(parseInt(p1, 16))));
 }
 
-sortData(column) {
-  let sortedData = this.parseDataObject(this.sortedDataobject);
-
-  sortedData.sort((a, b) => {
-    if (a[column] < b[column]) {
-      return this.sortDirection === 'asc' ? -1 : 1;
+  // LitElement connectedCallback lifecycle method
+  connectedCallback() {
+    super.connectedCallback();
+    
+    // Parse the data object
+    const data = this.parseDataObject();
+    if (!data) {
+      console.error('Invalid data object');
+      return;
     }
-    if (a[column] > b[column]) {
-      return this.sortDirection === 'asc' ? 1 : -1;
-    }
-    return 0;
-  });
-
-  this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
-  this.sortedDataobject = JSON.stringify(sortedData);
-}
-
-  changePage(newPage) {
-    if (newPage > 0 && newPage <= this.totalPages) {
-      this.currentPage = newPage;
-      this.requestUpdate();
-    }
+    
+    // Initialize Tabulator after the component is connected to the DOM
+    new Tabulator(this, {
+      data,
+      layout: 'fitDataFill',
+      pagination: 'local',
+      paginationSize: this.pageItemLimit,
+      paginationSizeSelector: [5, 10, 15, 30, 50, 100],
+      movableColumns: true,
+      height: 'auto',
+      autoColumns: true,
+    });
   }
 
   render() {
-    const data = this.parseDataObject();
-
-    if (!data || data.length === 0) {
-        return html`
-            <p>No Data Found</p>
-        `;
-    }
-
-    const startIndex = (this.currentPage - 1) * parseInt(this.pageItemLimit, 10);
-    const endIndex = startIndex + parseInt(this.pageItemLimit, 10);
-    const paginatedData = data.slice(startIndex, endIndex);
-    const totalPages = Math.ceil(data.length / parseInt(this.pageItemLimit, 10));
-    this.totalPages = totalPages; // Assign to component property
-
-    const rows = paginatedData.map(row => html`
-        <tr>
-            ${Object.keys(row).map(key => html`<td class="text-nowrap">${row[key]}</td>`)}
-        </tr>
-    `);
-
-    const headers = Object.keys(data[0]).map(header => html`
-        <th class="text-nowrap" @click="${() => this.updateSort(header)}">
-            ${header}
-            ${this.sortColumn === header ? (this.sortOrder === 'asc' ? '▲' : '▼') : ''}
-        </th>
-    `);
-
-    // Calculate the range of pages to display
-    const maxPagesToShow = 5;
-    const pageRange = Math.min(totalPages, maxPagesToShow);
-    let startPage = Math.max(1, this.currentPage - Math.floor(pageRange / 2));
-    const endPage = Math.min(totalPages, startPage + pageRange - 1);
-
-    // Adjust startPage if it exceeds the valid range
-    if (endPage - startPage + 1 < pageRange) {
-        startPage = Math.max(1, endPage - pageRange + 1);
-    }
-
-    return html`
-        <style>
-            .page-txt-link {
-                width: 100px;
-                text-align:center;
-            }
-            .page-num-link {
-                width: 45px;
-                text-align:center;
-            }
-        </style>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <div class="table-responsive-md overflow-auto">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        ${headers}
-                    </tr>
-                </thead>
-                <tbody>
-                    ${rows}
-                </tbody>
-            </table>
-        </div>
-        <div class="row">
-            ${totalPages > 1 ? html`
-                <nav aria-label="Page navigation">
-                    <ul class="pagination justify-content-center">
-                        <li class="page-item ${this.currentPage === 1 ? 'disabled' : ''}">
-                            <a class="page-link page-txt-link" href="#" @click="${() => this.changePage(1)}">First</a>
-                        </li>
-                        <li class="page-item ${this.currentPage === 1 ? 'disabled' : ''}">
-                            <a class="page-link page-txt-link" href="#" @click="${() => this.changePage(this.currentPage - 1)}">Previous</a>
-                        </li>
-                        ${Array.from({ length: endPage - startPage + 1 }, (_, i) => i + startPage).map(page => html`
-                            <li class="page-item ${page === this.currentPage ? 'active' : ''}">
-                                <a class="page-link page-num-link" href="#" @click="${() => this.changePage(page)}">${page}</a>
-                            </li>
-                        `)}
-                        <li class="page-item ${this.currentPage === totalPages ? 'disabled' : ''}">
-                            <a class="page-link page-txt-link" href="#" @click="${() => this.changePage(this.currentPage + 1)}">Next</a>
-                        </li>
-                        <li class="page-item ${this.currentPage === totalPages ? 'disabled' : ''}">
-                            <a class="page-link page-txt-link" href="#" @click="${() => this.changePage(totalPages)}">Last</a>
-                        </li>
-                    </ul>
-                </nav>
-            ` : ''}
-        </div>
-    `;
-}
-
-
+    return html`<div></div>`;
+  }
 }
 
 customElements.define('neo-listviewer', listviewElement);
