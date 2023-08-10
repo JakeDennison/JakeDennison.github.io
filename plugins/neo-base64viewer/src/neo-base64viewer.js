ec96dfc5b76cae38a097528efac8442891b8005f
JSDenintex <jake.dennison@nintex.com>
2023-08-10 15:13:54 +0100

    .

--- plugins/neo-base64viewer/src/neo-base64viewer.js
+++ plugins/neo-base64viewer/src/neo-base64viewer.js
@@ -8,12 +8,17 @@ class base64viewerElement extends LitElement {
       description: '',
       iconUrl: `https://jsdenintex.github.io/plugins/neo-base64viewer/dist/files/icon.svg`,
       groupName: 'Visual',
-      version: '1.0',
+      version: '1.1',
       properties: {
         base64Data: {
           type: 'string',
-          title: 'File as Base64',
-          description: 'Please convert the file into Base64 and enter the value here.'
+          title: 'Files as Base64',
+          description: 'Please provide base64-encoded strings for the files or a base64 string for a single file.',
+        },
+        fileNames: {
+          type: 'string',
+          title: 'File Names',
+          description: 'JSON array of file names corresponding to the base64Data. Required for navigation and multi-file display.',
         },
         pdfparam: {
           type: 'string',
@@ -25,13 +30,13 @@ class base64viewerElement extends LitElement {
           type: 'string',
           title: 'Height',
           description: 'use standard HTML height values, 80%, 720px, 4vh etc.',
-          defaultValue: '600px'
+          defaultValue: '600px',
         },
       },
       standardProperties: {
         fieldLabel: true,
         description: true,
-      }
+      },
     };
   }
 
@@ -47,8 +52,10 @@ class base64viewerElement extends LitElement {
   constructor() {
     super();
     this.base64Data = '';
+    this.fileNames = '';
     this.pdfparam = 'zoom=FitH';
     this.docheight = '600px';
+    this.activeIndex = 0;
   }
 
   render() {
@@ -56,12 +63,35 @@ class base64viewerElement extends LitElement {
       return html``;
     }
 
+    const base64Array = JSON.parse(this.base64Data);
     const pdfParams = this.pdfparam ? `#${this.pdfparam}` : '';
-    const dataUrl = `data:application/pdf;base64,${this.base64Data}`;
+    const iframeStyle = this.docheight ? `height: ${this.docheight};` : '';
 
-    const iframeStyle = this.docheight
-      ? `height: ${this.docheight};`
-      : '';
+    if (base64Array.length > 1) {
+      return html`
+        <iframe
+          src="${this.getPDFSrc(this.activeIndex, pdfParams, base64Array)}"
+          style="${iframeStyle}"
+          width="100%"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+        <nav>
+          ${base64Array.map(
+            (_, index) => html`
+              <button
+                @click="${() => (this.activeIndex = index)}"
+                ?active="${index === this.activeIndex}"
+              >
+                ${this.getFileName(index)}
+              </button>
+            `
+          )}
+        </nav>
+      `;
+    }
+
+    const dataUrl = `data:application/pdf;base64,${base64Array[0]}`;
 
     return html`
       <iframe
@@ -72,6 +102,18 @@ class base64viewerElement extends LitElement {
         allowfullscreen
       ></iframe>
     `;
+  }
+
+  getPDFSrc(index, params, base64Array) {
+    return `data:application/pdf;base64,${base64Array[index]}${params}`;
+  }
+
+  getFileName(index) {
+    if (!this.fileNames) {
+      return `File ${index + 1}`;
+    }
+    const names = JSON.parse(this.fileNames);
+    return names[index] || `File ${index + 1}`;
   }
 }
 
