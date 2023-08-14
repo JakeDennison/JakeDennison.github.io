45a784aca3244095152aa4fe4763b6ff2266a800
JSDenintex <jake.dennison@nintex.com>
2023-08-14 16:13:25 +0100

    .

--- plugins/neo-base64viewer/src/neo-base64viewer.js
+++ plugins/neo-base64viewer/src/neo-base64viewer.js
@@ -32,7 +32,21 @@ class base64viewerElement extends LitElement {
           description: 'use standard HTML height values, 80%, 720px, 4vh etc.',
           defaultValue: '600px',
         },
+        freezeinput: {
+          title: 'Freeze input',
+          description: 'Enable to prevent refresh on base64 value change.',
+          type: 'boolean',
+          defaultValue: false,
+        },
+        docloaded: {
+          title: 'Document displayed',
+          description: 'Enable to prevent refresh on base64 value change.',
+          type: 'boolean',
+          defaultValue: false,
+          isValueField: true,
+        },
       },
+      events: ["ntx-value-change"],
       standardProperties: {
         fieldLabel: true,
         description: true,
@@ -52,63 +66,92 @@ class base64viewerElement extends LitElement {
   constructor() {
     super();
     this.base64Data = '';
+    this.base64store = '';
     this.fileNames = '';
     this.pdfparam = 'zoom=FitH';
     this.docheight = '600px';
     this.activeIndex = 0;
-  }
-
-  render() {
-    if (!this.base64Data) {
-      return html``;
+    this.freezeinput = false;
+    this.docloaded = false;
+    this.initialLoad = true;
     }
 
-    let base64Array = [];
-    if (Array.isArray(this.base64Data)) {
-      base64Array = this.base64Data;
-    } else {
-      base64Array = [this.base64Data];
+    updated(changedProperties) {
+      if (changedProperties.has('base64Data')) {
+        if (this.freezeinput && this.base64store === '') {
+          this.base64store = this.base64Data;
+        } else if (!this.freezeinput) {
+          this.base64store = this.base64Data;
+        }
+      }
     }
 
-    const pdfParams = this.pdfparam ? `#${this.pdfparam}` : '';
-    const iframeStyle = this.docheight ? `height: ${this.docheight};` : '';
+    render() {
+      if (!this.base64store) {
+        return html``;
+      }
+    
+      let base64Array = [];
+      if (Array.isArray(this.base64store)) {
+        base64Array = this.base64store;
+      } else {
+        base64Array = [this.base64store];
+      }
+    
+      const pdfParams = this.pdfparam ? `#${this.pdfparam}` : '';
+      const iframeStyle = this.docheight ? `height: ${this.docheight};` : '';
+    
+      if (base64Array.length > 1) {
+        return html`
+          <iframe
+            src="${this.getPDFSrc(this.activeIndex, pdfParams, base64Array)}"
+            style="${iframeStyle}"
+            width="100%"
+            frameborder="0"
+            allowfullscreen
+            @load="${this.handleDocumentLoad}"
+          ></iframe>
+          <nav>
+            ${base64Array.map(
+              (_, index) => html`
+                <button
+                  @click="${() => (this.activeIndex = index)}"
+                  ?active="${index === this.activeIndex}"
+                >
+                  ${this.getFileName(index)}
+                </button>
+              `
+            )}
+          </nav>
+        `;
+      }
+    
+      const dataUrl = `data:application/pdf;base64,${base64Array[0]}`;
 
-    if (base64Array.length > 1) {
       return html`
         <iframe
-          src="${this.getPDFSrc(this.activeIndex, pdfParams, base64Array)}"
+          src="${dataUrl}${pdfParams}"
           style="${iframeStyle}"
           width="100%"
           frameborder="0"
           allowfullscreen
+          @load="${this.handleDocumentLoad}"
         ></iframe>
-        <nav>
-          ${base64Array.map(
-            (_, index) => html`
-              <button
-                @click="${() => (this.activeIndex = index)}"
-                ?active="${index === this.activeIndex}"
-              >
-                ${this.getFileName(index)}
-              </button>
-            `
-          )}
-        </nav>
       `;
     }
-
-    const dataUrl = `data:application/pdf;base64,${base64Array[0]}`;
-
-    return html`
-      <iframe
-        src="${dataUrl}${pdfParams}"
-        style="${iframeStyle}"
-        width="100%"
-        frameborder="0"
-        allowfullscreen
-      ></iframe>
-    `;
-  }
+    
+    
+    handleDocumentLoad() {
+      this.docloaded = true;
+      const args = {
+        bubbles: true,
+        cancelable: false,
+        composed: true,
+        detail: this.docloaded,
+      };
+      const event = new CustomEvent('ntx-value-change', args);
+      this.dispatchEvent(event);
+    }
 
   getPDFSrc(index, params, base64Array) {
     return `data:application/pdf;base64,${base64Array[index]}${params}`;
