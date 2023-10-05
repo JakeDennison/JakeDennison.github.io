dda3f40a035c1398e8c5904c70058f24e8712327
JSDenintex <jake.dennison@nintex.com>
2023-10-05 10:00:30 +0100

    .

--- src/js/iframe-docgen.js
+++ src/js/iframe-docgen.js
@@ -1,6 +1,6 @@
 function onLoad(executionContext) {
     var formContext = executionContext.getFormContext();
-    var iframeControl = formContext.getControl("IFRAME_NAME");
+    var iframeControl = formContext.getControl("IFRAME_docgen");
 
     if (iframeControl) {
         var iframeSrc = "https://demo-safalo-emea.workflowcloud.com/embedform/iframe?id=71137b22-e496-46b9-99a8-2f55f0dc9733&tenancy=demo-safalo-emea.workflowcloud.com";
