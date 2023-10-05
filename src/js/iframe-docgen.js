d1d1376cbb3dd78472ea67175e8df8fbb5ab5e1e
JSDenintex <jake.dennison@nintex.com>
2023-10-05 09:39:08 +0100

    added iframe doc-gen

---
+++ src/js/iframe-docgen.js
@@ -0,0 +1,11 @@
+function onLoad(executionContext) {
+    var formContext = executionContext.getFormContext();
+    var iframeControl = formContext.getControl("IFRAME_NAME");
+
+    if (iframeControl) {
+        var iframeSrc = "https://demo-safalo-emea.workflowcloud.com/embedform/iframe?id=71137b22-e496-46b9-99a8-2f55f0dc9733&tenancy=demo-safalo-emea.workflowcloud.com";
+        iframeControl.setSrc(iframeSrc);
+        
+        // Additional IFrame properties can be set here (like height, width, etc.) as per your requirements.
+    }
+}
