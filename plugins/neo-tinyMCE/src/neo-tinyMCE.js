6ae42d94e2521ad6d5d6076f1eecbe4b7e2e4c6f
JSDenintex <jake.dennison@nintex.com>
2023-11-10 10:30:15 +0000

    .

--- plugins/neo-tinyMCE/src/neo-tinyMCE.js
+++ plugins/neo-tinyMCE/src/neo-tinyMCE.js
@@ -42,6 +42,7 @@ class tinyMCEElement extends LitElement {
       <div>
       <script src="https://cdn.tiny.cloud/1/qagffr3pkuv17a8on1afax661irst1hbr4e6tbv888sz91jc/tinymce/6/tinymce.min.js"></script>
       <script>
+        console.log("script started")
         tinymce.init({
           selector: 'textarea#tiny-mce-editor',
           plugins: [
