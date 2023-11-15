14021f447a108b7fd40386084ca8508f87ee6062
JSDenintex <jake.dennison@nintex.com>
2023-11-15 11:26:22 +0000

    .

---
+++ plugins/neo-express/src/neo-express.js
@@ -0,0 +1,77 @@
+import { LitElement } from 'lit';
+
+class expressElement extends LitElement {
+  static getMetaConfig() {
+    // plugin contract information
+    return {
+      controlName: 'neo-express',
+      fallbackDisableSubmit: false,
+      description: 'Allow for instant form submission based on rules',
+      iconUrl: "boolean-icon",
+      groupName: 'Admin tools',
+      version: '1.0',
+      properties: {
+        oTarget: {
+          type: 'string',
+          title: 'Outcome Target CSS Class',
+          description: 'Apply a CSS class to the outcome control and provide it here'
+        },
+        oValue: {
+          type: 'string',
+          title: 'Outcome Value',
+          description: 'Provide the value for the outcome.'
+        },
+        oBool: {
+          type: 'boolean',
+          title: 'Enabled',
+          description: 'If true the form will submit on load, if false it will wait for rule to set the value to true'
+        }
+      },
+      standardProperties: {
+        fieldLabel: true,
+        description: true,
+      }
+    };
+  }
+
+  static properties = {
+    oTarget: '',
+    oValue: '',
+    oBool: { type: Boolean },
+  };
+
+  constructor() {
+    super();
+    this.oTarget = '';
+    this.oValue = '';
+    this.oBool = false;
+  }
+
+  firstUpdated(changedProperties) {
+    super.firstUpdated(changedProperties);
+
+    // Run the logic on load if oBool is true
+    if (this.oBool) {
+      this._updateRadioControlValue();
+    }
+  }
+
+  updated(changedProperties) {
+    super.updated(changedProperties);
+
+    // Check if oBool has changed and is now true
+    if (changedProperties.has('oBool') && this.oBool) {
+      this._updateRadioControlValue();
+    }
+  }
+
+  _updateRadioControlValue() {
+    // Update the radio control's value when needed
+    const radioControl = this.querySelector(`.${this.oTarget}`);
+    if (radioControl) {
+      radioControl.value = this.oValue;
+    }
+  }
+}
+
+customElements.define('neo-express', expressElement);
