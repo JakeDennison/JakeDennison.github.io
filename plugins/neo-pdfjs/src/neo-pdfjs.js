9d9d17b8bd6202b5759abf554e3d763023f4d684
JSDenintex <jake.dennison@nintex.com>
2023-11-17 14:07:41 +0000

    .

--- plugins/neo-pdfjs/src/neo-pdfjs.js
+++ plugins/neo-pdfjs/src/neo-pdfjs.js
@@ -1,10 +1,4 @@
 import { LitElement, html, css } from 'lit';
-import {
-  getDocument,
-  PDFWorker,
-  // ... other named exports you need
-} from 'pdfjs-dist';
-
 
 class pdfjsElement extends LitElement {
   static getMetaConfig() {
@@ -47,18 +41,50 @@ class pdfjsElement extends LitElement {
     this.src = '';
   }
 
+  async connectedCallback() {
+    super.connectedCallback();
+    this.toggleSpinner(true);
+
+    // Check if PDF.js script is already loaded
+    if (!window.pdfjsLib) {
+      console.log("PDF.js script being added");
+      await this.loadPDFJSScript();
+    }
+
+    if (window.pdfjsLib) {
+      // PDF.js is loaded, you can now use it
+      this.loadPDF('your-pdf-url.pdf');
+    }
+  }
+
+  loadPDFJSScript() {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.min.js'; // Replace with your preferred CDN URL
+      script.onload = () => {
+        console.log('PDF.js script loaded');
+        resolve();
+      };
+      script.onerror = reject;
+      document.head.appendChild(script);
+    });
+  }
+
   async loadPDF(pdfUrl) {
+    const pdfjsLib = window.pdfjsLib;
+    
     try {
-      const pdfDoc = await pdfjsLib.getDocument({ url: pdfUrl, worker: new PDFWorker() }).promise;
+      const pdfDoc = await pdfjsLib.getDocument(pdfUrl).promise;
       const pdfPage = await pdfDoc.getPage(1);
       const canvas = this.shadowRoot.querySelector('canvas');
       const context = canvas.getContext('2d');
-  
+
       const viewport = pdfPage.getViewport({ scale: 1.5 });
       canvas.width = viewport.width;
       canvas.height = viewport.height;
-  
+
       await pdfPage.render({ canvasContext: context, viewport });
+
     } catch (error) {
       console.error('Error loading PDF:', error);
     }
