598e600b90312a214066440fd60d6a6410e5fa1b
JSDenintex <jake.dennison@nintex.com>
2024-02-29 09:57:17 +0000

    adding ww demo files

---
+++ src/js/ww.js
@@ -0,0 +1,22 @@
+// Array of SVG paths for each step
+const svgPaths = [
+    '/img/consumer/svg/problem1.svg',
+    '/img/consumer/svg/problem2.svg',
+    '/img/consumer/svg/problem3.svg',
+    '/img/consumer/svg/problem4.svg'
+  ];
+  
+  // Get all the spans containing numbers in the stepper
+  const numberSpans = document.querySelectorAll('.mat-step-icon.mat-step-icon-state-number .mat-step-icon-content .ng-star-inserted');
+  
+  // Iterate over each span and replace its content with the corresponding SVG
+  numberSpans.forEach((span, index) => {
+    // Create a new SVG element
+    const svg = document.createElement('img');
+    // Set the SVG source based on the index
+    svg.src = svgPaths[index];
+    // Replace the content of the span with the SVG
+    span.innerHTML = '';
+    span.appendChild(svg);
+  });
+  
