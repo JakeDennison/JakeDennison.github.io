a7f71755cb89a683cad85d3a0ee02938c9a4f363
JSDenintex <jake.dennison@nintex.com>
2024-03-19 15:56:25 +0000

    added tabulator tables

---
+++ plugins/neo-tabulator/src/neo-template.js
@@ -0,0 +1,51 @@
+import { LitElement, html, css } from 'lit';
+import Tabulator from 'tabulator-tables'; // Import Tabulator (make sure to install it first or include it in your project)
+import 'tabulator-tables/dist/css/tabulator.min.css'; // Import Tabulator CSS
+
+class TabulatorElement extends LitElement {
+  static properties = {
+    src: { type: String },
+  };
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+    `;
+  }
+  
+  constructor() {
+    super();
+    this.src = '';
+  }
+
+  firstUpdated() {
+    this._initializeTable();
+  }
+
+  updated(changedProperties) {
+    if (changedProperties.has('src') && this.src) {
+      this._updateTable();
+    }
+  }
+
+  _initializeTable() {
+    this.table = new Tabulator(this.shadowRoot.getElementById('tabulator'), {
+      data: JSON.parse(this.src), // Set data from the `src` property
+      autoColumns: true, // Automatically create columns from data keys
+      layout: "fitColumns", // Fit columns to width of table
+    });
+  }
+
+  _updateTable() {
+    const data = JSON.parse(this.src);
+    this.table.setData(data);
+  }
+
+  render() {
+    return html`<div id="tabulator"></div>`; // Container for the Tabulator table
+  }
+}
+
+customElements.define('neo-tabulator', TabulatorElement);
---
+++ plugins/neo-tabulator/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/neo-template.js',
+  mode: 'production',
+  output: {
+    filename: 'neo-template.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
