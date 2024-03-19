141712da7b358de5691a3beae1e79f38f49df377
JSDenintex <jake.dennison@nintex.com>
2024-03-19 15:23:27 +0000

    .

--- plugins/neo-table/neo-table-viewer.js
+++ plugins/neo-table/neo-table-viewer.js
@@ -279,131 +279,138 @@ export class MyTable extends LitElement {
     }
   }
 
-
-
   render() {
     const data = this.parseDataObject();
-
+  
     // Display error message if present
     if (this.errorMessage) {
       return html`<p class="error-message">${this.errorMessage}</p>`;
     }
-
+  
     if (!data || data.length === 0) {
       return html`
-      <div class="alert alert-secondary" role="alert">
-        No Data Found
-      </div>
-    `;
+        <div class="alert alert-secondary" role="alert">
+          No Data Found
+        </div>
+      `;
     }
-
+  
     const startIndex = (this.currentPage - 1) * parseInt(this.pageItemLimit, 10);
     const endIndex = startIndex + parseInt(this.pageItemLimit, 10);
     const paginatedData = data.slice(startIndex, endIndex);
     const totalPages = Math.ceil(data.length / parseInt(this.pageItemLimit, 10));
     this.totalPages = totalPages; // Assign to component property
-
+  
     // Calculate the range of pages to display
     const maxPagesToShow = 5;
     const pageRange = Math.min(totalPages, maxPagesToShow);
     let startPage = Math.max(1, this.currentPage - Math.floor(pageRange / 2));
     const endPage = Math.min(totalPages, startPage + pageRange - 1);
-
+  
     // Adjust startPage if it exceeds the valid range
     if (endPage - startPage + 1 < pageRange) {
       startPage = Math.max(1, endPage - pageRange + 1);
     }
-
+  
     return html`
-    <style>
-      .page-txt-link {
-        width: 100px;
-        text-align:center;
-      }
-      .page-num-link {
-        width: 45px;
-        text-align:center;
-      }
-      .nested-table{
-        column-span: all;
-      }
-      .nested-list {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-      }
-      .neo-table {
-        -moz-user-select: text;
-        -khtml-user-select: text;
-        -webkit-user-select: text;
-        -ms-user-select: text;
-        user-select: text;
-      }
-    </style>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <div class="table-responsive-md overflow-auto">
-      <table class="neo-table table table-striped">
-        <thead>
-          <tr>
-            ${Object.keys(data[0]).map(header => html`<th class="text-nowrap">${header}</th>`)}
-          </tr>
-        </thead>
-        <tbody>
-          ${paginatedData.map(row => html`
+      <style>
+        .page-txt-link {
+          width: 100px;
+          text-align:center;
+        }
+        .page-num-link {
+          width: 45px;
+          text-align:center;
+        }
+        .nested-table{
+          column-span: all;
+        }
+        .nested-list {
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+        }
+        .neo-table {
+          -moz-user-select: text;
+          -khtml-user-select: text;
+          -webkit-user-select: text;
+          -ms-user-select: text;
+          user-select: text;
+        }
+      </style>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+      <div class="table-responsive-md overflow-auto">
+        <table class="neo-table table table-striped">
+          <thead>
             <tr>
-              ${Object.values(row).map(value => html`
-                <td>
-                  ${Array.isArray(value) ?
-        html`
-                      <table class="table mb-2 p-1">
-                        <tbody>
-                          ${value.map(item => html`
-                            <tr>
-                              <td>${this.renderField(item)}</td>
-                            </tr>
-                          `)}
-                        </tbody>
-                      </table>
-                    ` :
-        html`${this.renderField(value)}`
-      }
-                </td>
-              `)}
+              ${Object.keys(data[0]).map(header => html`<th class="text-nowrap">${header}</th>`)}
             </tr>
-          `)}
-        </tbody>
-      </table>
-    </div>
-    <div class="row">
-      ${totalPages > 1 ? html`
-        <nav aria-label="Page navigation">
-          <ul class="pagination justify-content-center">
-            <li class="page-item ${this.currentPage === 1 ? 'disabled' : ''}">
-              <a class="page-link page-txt-link" href="#" @click="${() => this.changePage(1)}">First</a>
-            </li>
-            <li class="page-item ${this.currentPage === 1 ? 'disabled' : ''}">
-              <a class="page-link page-txt-link" href="#" @click="${() => this.changePage(this.currentPage - 1)}">Previous</a>
-            </li>
-            ${Array.from({ length: endPage - startPage + 1 }, (_, i) => i + startPage).map(page => html`
-              <li class="page-item ${page === this.currentPage ? 'active' : ''}">
-                <a class="page-link page-num-link" href="#" @click="${() => this.changePage(page)}">${page}</a>
-              </li>
+          </thead>
+          <tbody>
+            ${paginatedData.map(row => html`
+              <tr>
+                ${Object.values(row).map(value => html`
+                  <td>${this.renderField(value)}</td>
+                `)}
+              </tr>
+              <tr>
+                <td colspan="${Object.keys(row).length}">
+                  ${this.renderNestedTables(row)}
+                </td>
+              </tr>
             `)}
-            <li class="page-item ${this.currentPage === totalPages ? 'disabled' : ''}">
-              <a class="page-link page-txt-link" href="#" @click="${() => this.changePage(this.currentPage + 1)}">Next</a>
-            </li>
-            <li class="page-item ${this.currentPage === totalPages ? 'disabled' : ''}">
-              <a class="page-link page-txt-link" href="#" @click="${() => this.changePage(totalPages)}">Last</a>
-            </li>
-          </ul>
-        </nav>
-      ` : ''}
-    </div>
-  `;
+          </tbody>
+        </table>
+      </div>
+      <div class="row">
+        ${totalPages > 1 ? html`
+          <nav aria-label="Page navigation">
+            <ul class="pagination justify-content-center">
+              <li class="page-item ${this.currentPage === 1 ? 'disabled' : ''}">
+                <a class="page-link page-txt-link" href="#" @click="${() => this.changePage(1)}">First</a>
+              </li>
+              <li class="page-item ${this.currentPage === 1 ? 'disabled' : ''}">
+                <a class="page-link page-txt-link" href="#" @click="${() => this.changePage(this.currentPage - 1)}">Previous</a>
+              </li>
+              ${Array.from({ length: endPage - startPage + 1 }, (_, i) => i + startPage).map(page => html`
+                <li class="page-item ${page === this.currentPage ? 'active' : ''}">
+                  <a class="page-link page-num-link" href="#" @click="${() => this.changePage(page)}">${page}</a>
+                </li>
+              `)}
+              <li class="page-item ${this.currentPage === totalPages ? 'disabled' : ''}">
+                <a class="page-link page-txt-link" href="#" @click="${() => this.changePage(this.currentPage + 1)}">Next</a>
+              </li>
+              <li class="page-item ${this.currentPage === totalPages ? 'disabled' : ''}">
+                <a class="page-link page-txt-link" href="#" @click="${() => this.changePage(totalPages)}">Last</a>
+              </li>
+            </ul>
+          </nav>
+        ` : ''}
+      </div>
+    `;
   }
-
-
-
+  
+  renderNestedTables(row) {
+    const nestedTables = Object.entries(row)
+      .filter(([key, value]) => Array.isArray(value))
+      .map(([key, value]) => html`
+        <table class="table mb-2 p-1">
+          <tbody>
+            <tr>
+              <th>${key}</th>
+            </tr>
+            ${value.map(item => html`
+              <tr>
+                <td>${this.renderField(item)}</td>
+              </tr>
+            `)}
+          </tbody>
+        </table>
+      `);
+  
+    return html`${nestedTables}`;
+  }
+  
 
 }
 
