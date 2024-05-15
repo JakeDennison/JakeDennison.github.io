35b2b08bcb5708e0b2bbd41791d34ccb3ab3c3d4
JSDenintex <jake.dennison@nintex.com>
2024-05-15 14:42:22 +0200

    .

--- plugins/neo-anno/src/neo-anno.js
+++ plugins/neo-anno/src/neo-anno.js
@@ -1,5 +1,5 @@
 import { html, LitElement, css } from 'lit';
-import { fabric } from 'fabric';
+import Konva from 'konva';
 
 class AnnoElement extends LitElement {
   static getMetaConfig() {
@@ -52,41 +52,87 @@ class AnnoElement extends LitElement {
   constructor() {
     super();
     this.src = 'https://jsdenintex.github.io/plugins/neo-anno/dist/img/person.png';
+    this.stage = null;
+    this.layer = null;
   }
 
   firstUpdated() {
-    this.setupCanvas();
+    this.setupKonva();
   }
 
-  setupCanvas() {
-    const canvasElement = this.shadowRoot.getElementById('canvas');
-    const canvas = new fabric.Canvas(canvasElement);
-
-    fabric.Image.fromURL(this.src, (img) => {
-      canvas.setWidth(img.width);
-      canvas.setHeight(img.height);
-      canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas));
+  setupKonva() {
+    const container = this.shadowRoot.getElementById('container');
+    
+    // Initialize Konva stage and layer
+    this.stage = new Konva.Stage({
+      container: container,
+      width: container.clientWidth,
+      height: container.clientHeight,
     });
 
-    this.canvas = canvas;
+    this.layer = new Konva.Layer();
+    this.stage.add(this.layer);
 
-    // Add simple drawing capabilities
-    canvas.isDrawingMode = true;
-    canvas.freeDrawingBrush.color = "red";
-    canvas.freeDrawingBrush.width = 5;
+    // Load the image
+    const img = new Image();
+    img.src = this.src;
+    img.onload = () => {
+      const konvaImage = new Konva.Image({
+        x: 0,
+        y: 0,
+        image: img,
+        width: img.width,
+        height: img.height,
+      });
 
-    // Save canvas state
-    canvas.on('path:created', () => {
-      this.image = canvas.toDataURL();
+      // Adjust stage size to image size
+      this.stage.width(img.width);
+      this.stage.height(img.height);
+
+      this.layer.add(konvaImage);
+      this.layer.draw();
+      
+      // Add drawing functionality
+      this.addDrawingFunctionality();
+    };
+  }
+
+  addDrawingFunctionality() {
+    let isDrawing = false;
+    let lastLine;
+
+    this.stage.on('mousedown touchstart', () => {
+      isDrawing = true;
+      const pos = this.stage.getPointerPosition();
+      lastLine = new Konva.Line({
+        stroke: 'red',
+        strokeWidth: 5,
+        globalCompositeOperation: 'source-over',
+        points: [pos.x, pos.y],
+      });
+      this.layer.add(lastLine);
+    });
+
+    this.stage.on('mousemove touchmove', () => {
+      if (!isDrawing) {
+        return;
+      }
+      const pos = this.stage.getPointerPosition();
+      const newPoints = lastLine.points().concat([pos.x, pos.y]);
+      lastLine.points(newPoints);
+      this.layer.batchDraw();
+    });
+
+    this.stage.on('mouseup touchend', () => {
+      isDrawing = false;
+      this.image = this.stage.toDataURL();
       this.requestUpdate();
     });
   }
 
   render() {
     return html`
-      <div class="image-container">
-        <canvas id="canvas"></canvas>
-      </div>
+      <div id="container" class="image-container"></div>
     `;
   }
 }
