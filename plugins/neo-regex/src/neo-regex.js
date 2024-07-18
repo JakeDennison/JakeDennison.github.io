b7ad653bfe10745e79aa24a7f4f24d939e56f2dc
JSDenintex <78734553+JSDenintex@users.noreply.github.com>
2024-07-18 09:11:10 -0700

    .

--- plugins/neo-regex/src/neo-regex.js
+++ plugins/neo-regex/src/neo-regex.js
@@ -1,6 +1,6 @@
 import { LitElement, html, css } from 'lit';
 
-class templateElement extends LitElement {
+class NeoRegex extends LitElement {
   static getMetaConfig() {
     // plugin contract information
     return {
@@ -16,14 +16,14 @@ class templateElement extends LitElement {
           title: 'Input Text',
           description: 'Please provide the input string you want to interrogate'
         },
-        Operation: {
+        operation: {
           title: 'Choice field',
           type: 'string',
           enum: ['Check Match', 'Split', 'Extract', 'Group', 'Replace'],
           showAsRadio: true,
           verticalLayout: true,
         },
-        Pattern: {
+        pattern: {
           type: 'string',
           title: 'Pattern',
           description: 'Regex Pattern'
@@ -33,13 +33,18 @@ class templateElement extends LitElement {
           type: 'boolean',
           defaultValue: true,
         },
+        replacementText: {
+          type: 'string',
+          title: 'Replacement Text',
+          description: 'Text to replace matches (used in Replace operation)'
+        },
         outputObject: {
           title: 'Output Object',
           type: 'object',
-          description: 'Please dont use',
+          description: 'Please do not use',
           isValueField: true,
           properties: {
-            value:{
+            value: {
               type: 'string',
               title: 'value',
             }
@@ -54,9 +59,16 @@ class templateElement extends LitElement {
     };
   }
 
-  static properties = {
-    src: '',
-  };
+  static get properties() {
+    return {
+      inputText: { type: String },
+      operation: { type: String },
+      pattern: { type: String },
+      ignoreCase: { type: Boolean },
+      replacementText: { type: String },
+      outputObject: { type: Object }
+    };
+  }
 
   static get styles() {
     return css`
@@ -65,25 +77,68 @@ class templateElement extends LitElement {
       }
     `;
   }
-  
+
   constructor() {
     super();
+    this.inputText = '';
+    this.operation = 'Check Match';
+    this.pattern = '';
+    this.ignoreCase = true;
+    this.replacementText = '';
+    this.outputObject = { value: '' };
   }
 
-  onChange(e) {
-    const args = {
-        bubbles: true,
-        cancelable: false,
-        composed: true,
-        // value coming from input change event. 
-        detail: e.target.value,
-    };
-    const event = new CustomEvent('ntx-value-change', args);
-    this.dispatchEvent(event);
-}
+  updated(changedProperties) {
+    if (changedProperties.has('inputText') || changedProperties.has('operation') ||
+        changedProperties.has('pattern') || changedProperties.has('ignoreCase') ||
+        changedProperties.has('replacementText')) {
+      this.performRegexOperation();
+    }
+  }
 
-  render() {''
+  performRegexOperation() {
+    const regex = new RegExp(this.pattern, this.ignoreCase ? 'i' : '');
+    let result = '';
+
+    switch (this.operation) {
+      case 'Check Match':
+        result = regex.test(this.inputText).toString();
+        break;
+      case 'Split':
+        result = this.inputText.split(regex).join(', ');
+        break;
+      case 'Extract':
+        const match = this.inputText.match(regex);
+        result = match ? match.join(', ') : '';
+        break;
+      case 'Group':
+        const groups = this.inputText.match(regex);
+        result = groups ? groups.slice(1).join(', ') : '';
+        break;
+      case 'Replace':
+        result = this.inputText.replace(regex, this.replacementText);
+        break;
+      default:
+        result = 'Invalid operation';
+    }
+
+    this.outputObject = { value: result };
+    this.dispatchEvent(new CustomEvent('ntx-value-change', {
+      detail: this.outputObject,
+      bubbles: true,
+      cancelable: false,
+      composed: true,
+    }));
+  }
+
+  render() {
+    return html`
+      <div>
+        <label>Output:</label>
+        <pre>${this.outputObject.value}</pre>
+      </div>
+    `;
   }
 }
 
-customElements.define('neo-regex', regexElement);
+customElements.define('neo-regex', NeoRegex);
